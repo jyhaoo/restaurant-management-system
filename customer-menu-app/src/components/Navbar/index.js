4d22935086759a48a10131e36fd90e26b473cef3
Jason Hao <jyhao1998@gmail.com>
2024-03-05 17:11:06 -0800

    style: update logo & removed image

--- customer-menu-app/src/components/Navbar/index.js
+++ customer-menu-app/src/components/Navbar/index.js
@@ -1,14 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import styled from "styled-components";
 import { IoCartOutline } from "react-icons/io5";
-
-const Img = styled.img`
-  width: 100px;
-  height: 65px;
-  margin-right: 10px;
-`;
 
 const CartStyle = { width: "40px", height: "40px" };
 
@@ -17,10 +10,7 @@ const NavBar = () => {
     <>
       <Navbar bg="light" data-bs-theme="light" sticky="top">
         <Container>
-          <Img
-            src={require("../../images/RestaurantName.png")}
-            alt="Restaurant Logo"
-          />
+          <Navbar.Brand>面条</Navbar.Brand>
           <Navbar.Brand href="#home">Mian Tsao</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/home">Home</Nav.Link>
