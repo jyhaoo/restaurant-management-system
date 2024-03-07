c4e0feedd1ad9bbb20350d353f0fc24e48ea7982
Jason Hao <jyhao1998@gmail.com>
2024-03-07 14:39:37 -0800

    style: add modal

---
+++ customer-menu-app/src/components/Modal/Modal.js
@@ -0,0 +1,33 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 550px;
+  height: 400px;
+`;
+
+function FoodModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.dishes.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ textAlign: "center" }}>
+        <Image src={require("../../images/ColdNoodles.jpeg")} />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button>Add to cart</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default FoodModal;
