16c323af73f14fe79a72b7fb4f3cccdaac34599e
Jason Hao <jyhao1998@gmail.com>
2024-03-06 16:43:03 -0800

    style: update menu item style

--- customer-menu-app/src/components/MenuItem/MenuItem.js
+++ customer-menu-app/src/components/MenuItem/MenuItem.js
@@ -1,53 +1,30 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 90%;
-  height: 400px;
-  display: flex;
-  margin-left: 5%;
-  margin-top: 10px;
-  border-radius: 25px;
-  border: 2px solid black;
-  display: flex;
-`;
-
-const InfoContainer = styled.div`
-  margin: 10px;
-  width: 50%;
-  border-radius: 25px;
-  display: box;
-`;
-
-const Name = styled.div`
-  font-size: 45px;
-  font-weight: 600;
-`;
-
-const Info = styled.div`
-  font-size: 18px;
-  height: 40px;
-`;
-
-const Price = styled.div`
-  margin-top: 30px;
-  font-size: 26px;
+  width: 400px;
+  height: 300px;
 `;
 
 const Img = styled.img`
-  width: 45%;
-  height: 95%;
-  margin: 10px;
+  width: 100%;
+  height: 85%;
+  border-radius: 25px;
 `;
 
-const MenuItem = () => {
+const Name = styled.div`
+  font-size: 18px;
+`;
+
+const Price = styled.div`
+  font-size: 15px;
+`;
+
+const MenuItem = ({ props }) => {
   return (
     <Container>
-      <InfoContainer>
-        <Name>Oyster Dynamite</Name>
-        <Info>Two 1/2 shells</Info>
-        <Price>$14.99</Price>
-      </InfoContainer>
       <Img src={require("../../images/ColdNoodles.jpeg")} />
+      <Name>{props.name}</Name>
+      <Price>{props.price}</Price>
     </Container>
   );
 };
--- customer-menu-app/src/pages/Menu.js
+++ customer-menu-app/src/pages/Menu.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import MenuItem from "../components/MenuItem/MenuItem";
 
@@ -6,11 +7,55 @@ const MenuName = styled.div`
   text-align: center;
 `;
 
+const MenuItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+`;
+
+const MenuItemWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 28px;
+  flex-wrap: wrap;
+`;
+
+const DummyData = {
+  name: "Oyster Dynamite",
+  price: 14.99,
+};
+
+const Data = [
+  {
+    name: "Appetizers",
+    id: "appitizers",
+    dishes: [
+      {
+        name: "Cucumber Salad",
+        price: 14.99,
+      },
+    ],
+  },
+];
+
 const Menu = () => {
+  const [openModal, setOpenModal] = useState({ state: false, menuItem: null });
   return (
     <>
       <MenuName id="hotdishes">Hot Dishes</MenuName>
-      <MenuItem />
+      <MenuItemContainer>
+        <MenuItemWrapper>
+          <MenuItem
+            props={DummyData}
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+          />{" "}
+        </MenuItemWrapper>
+      </MenuItemContainer>
     </>
   );
 };
