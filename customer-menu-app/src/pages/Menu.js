5e2a0be657ecbe9dde5c8f5275a051d6bbd58d22
Jason Hao <jyhao1998@gmail.com>
2024-03-06 17:18:47 -0800

    test: add dummy data

--- customer-menu-app/src/pages/Menu.js
+++ customer-menu-app/src/pages/Menu.js
@@ -36,8 +36,31 @@ const Data = [
     dishes: [
       {
         name: "Cucumber Salad",
-        price: 14.99,
+        price: 7.99,
       },
+      { name: "Soy Noodle Salad", price: 7.99 },
+      { name: "Wood Ear Mushrooms", price: 8.99 },
+      { name: "Sweet & Sour Pork Baby Back Ribs", price: 10.99 },
+    ],
+  },
+  {
+    name: "Xiao Long Bao",
+    id: "xiaolongbao",
+    dishes: [
+      { name: "Kurobuta Pork Xiao Long Bao", price: 13.99 },
+      { name: "Crab & Kurobuta Pork Xiao Long Bao", price: 13.99 },
+      { name: "Chicken Xiao Long Bao", price: 13.49 },
+    ],
+  },
+  {
+    name: "Steamed Dumplings & Buns",
+    id: "dumplings&buns",
+    dishes: [
+      { name: "Chicken Dumplings", price: 11.99 },
+      { name: "Vegan Dumplings", price: 11.99 },
+      { name: "Cod Dumplings", price: 11.99 },
+      { name: "Shrimp & Kurobuta Pork Dumplings", price: 12.99 },
+      { name: "Kimchi & Kurobuta Pork Dumplings", price: 12.99 },
     ],
   },
 ];
@@ -53,7 +76,7 @@ const Menu = () => {
             props={DummyData}
             openModal={openModal}
             setOpenModal={setOpenModal}
-          />{" "}
+          />
         </MenuItemWrapper>
       </MenuItemContainer>
     </>
