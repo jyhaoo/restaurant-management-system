0e833458df5691a0a9f50c22d400a748a810a342
Jason Hao <jyhao1998@gmail.com>
2024-03-07 16:42:54 -0800

    Initial commit

---
+++ restaurant-management-system-backend/src/test/java/com/jyhaoo/restaurantmanagementsystembackend/RestaurantManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhaoo.restaurantmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
